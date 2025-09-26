9b18400628efdc082aa7be47873873538e2e9769
M1ghty-Disciple <henlonrandy44@gmail.com>
2025-09-26 17:35:30 -0500

    hw2

--- Homework2/main.java
+++ Homework2/main.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 public class main {
 
     public static void menu() {
-        System.out.println("==== Library Menu ==== ");
+        System.out.println("\n==== Library Menu ==== ");
         System.out.println("1 - View Available Items \n2 - Borrow an Item\n3 - Exit");
         System.out.println("Choose an option 1-3");
     }
 
-    public static void showItems(LibraryItem[] items){
-        for(int i = 0; i < items.length; i++){
-            items[i].displayDetails();
+    public static void showItems(LibraryItem[] items) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null)
+                continue;
+            else
+                items[i].displayDetails();
         }
     }
-
-   
 
     public static void main(String[] args) {
         // Storing all the objects in the library
@@ -26,103 +27,70 @@ public class main {
         availableItems[3] = new DVD("D301", "Interstellar", 2.00, "169 min", "PG-13");
         availableItems[4] = new DVD("D302", "Inception", 2.00, "148 min", "PG-13");
 
-       
+        boolean exit = false;
         Scanner input = new Scanner(System.in);
-        
+        int choice = 0;
 
-        
-        
-            menu();  
-            int choice = input.nextInt();  
-            //switch statement to handle different user inputs
-            switch(choice){
-                //View Available Items
+        while (!exit) {
+            menu();// shows the menu
+
+            try {
+                choice = input.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Improper input");
+                break;// I couldn't figure out how to keep the loop running when this error occured
+                      // here
+
+            }
+
+            // switch statement to handle different user inputs
+            switch (choice) {
+                // View Available Items
                 case 1:
                     showItems(availableItems);
                     break;
 
-                //Borrow an item
+                // Borrow an item
                 case 2:
-                    
-                    try{
+                    input.nextLine(); // Clear the newline character from the stream
+                    try {
                         System.out.print("Enter item ID to borrow: ");
                         String id = input.nextLine();
 
-                        System.out.println("Enter the number of days");
+                        System.out.print("Enter the number of days: ");
                         int days = input.nextInt();
 
-                        for(int i = 0; i < availableItems.length; i++){
-                            if(availableItems[i].getItemId().equalsIgnoreCase(id)){
-                                System.out.printf("You borrowed %s for %d days. Total cost is %d", 
-                                availableItems[i].getTitle(), days, availableItems[i].calculateBorrowCost(days));
+                        for (int i = 0; i < availableItems.length; i++) {
+                            if (availableItems[i] == null)
+                                continue;
+
+                            else if (availableItems[i].getItemId().equalsIgnoreCase(id)) {
+                                System.out.printf("You borrowed '%s' for %d days. Total cost is $%.2f\n",
+                                        availableItems[i].getTitle(), days,
+                                        availableItems[i].calculateBorrowCost(days));
 
                                 availableItems[i] = null;
                                 System.out.println("Item is now marked as borrowed and no longer available");
+                            }
                         }
-                    }
-                    }catch(InputMismatchException e){
-                        System.out.println("There seems to be an error in yout input");
+                    } catch (InputMismatchException e) {
+                        System.out.println("There seems to be an error in your input");
                     }
                     break;
-                    
-                    //test whether .equals can handle null
-                    
-                //Exit
+
+                // Exit
                 case 3:
                     System.out.println("Thank you for stopping by");
-                    
+                    exit = true;
                     break;
 
                 default:
-                        System.out.println("You must enter either 1, 2 or 3");
-                        break;
+                    System.out.println("You must enter either 1, 2 or 3");
+                    break;
 
-            }//End of switch Stmt
+            }// End of switch Stmt
 
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
 
     }
 }
